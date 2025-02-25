c7fa7f43fad0c7f7221b61bd92eac5df192a8f8b
ro77en <lucas.mdantas@icloud.com>
2025-02-25 10:32:41 -0300

    feat: add getUppercasedName method in User class

--- src/model/User.java
+++ src/model/User.java
@@ -54,4 +54,8 @@ public class User {
                 ", height=" + height +
                 '}';
     }
+
+    public String getUppercasedName() {
+        return this.name.toUpperCase();
+    }
 }
